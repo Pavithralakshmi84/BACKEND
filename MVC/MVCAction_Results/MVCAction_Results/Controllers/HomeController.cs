55babf15b217a7d8d70305ffdaf4ff7ce0d906c6
unknown <pavithralakshmi84@gmail.com>
2026-03-17 10:39:54 +0530

    ActionView

---
+++ MVC/MVCAction_Results/MVCAction_Results/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAction_Results.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Action Method 2
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // 3. Third action (view in Shared folder)
+        public ActionResult SharedPage()
+        {
+            return View("~/Views/Shared/SharedPage.cshtml");
+        }
+
+        // 4. Fourth action (view in Test folder)
+        public ActionResult TestPage()
+        {
+            return View("~/Views/Test/TestPage.cshtml");
+        }
+
+        // 5. Fifth action (another view in Test folder)
+        public ActionResult CustomPage()
+        {
+            return View("~/Views/Test/CustomPage.cshtml");
+        }
+    }
+}
---
+++ MVC/MVCAction_Results/MVCAction_Results/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCAction_Results.MvcApplication" Language="C#" %>
